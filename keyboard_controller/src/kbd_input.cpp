aabd46126c360ab80409f4c18f3d74f671b42379
orlandolara22 <orlando21lara@gmail.com>
2020-05-23 21:16:02 -0500

    Changed keyboard_controller node so that velocities are hard coded in an array of rpm values for linear and angular domain. Also added a forked lama (mapping algorithm) repositories as a submodule

--- keyboard_controller/src/kbd_input.cpp
+++ keyboard_controller/src/kbd_input.cpp
@@ -18,7 +18,8 @@ const float font_scale	    = 2;
 const int text_thickness    = 2;
 const int text_font	    = cv::FONT_HERSHEY_PLAIN;
 const int line_type	    = cv::FILLED;
-const cv::Scalar text_color = cv::Scalar(79,14,150);
+//const cv::Scalar text_color = cv::Scalar(79,14,150);
+const cv::Scalar text_color = cv::Scalar(0,0,0);
 
 const cv::Point text_origin		= cv::Point(110,30);
 const cv::Point speed_lvl_text_origin   = cv::Point(0,70);
@@ -28,17 +29,33 @@ const cv::Point units_text_origin	= cv::Point(300,110);
 const float DIST_BETWEEN_WHEELS	= 0.225;  // This is the distance between the wheels in m
 const float WHEEL_RADIUS	= 0.0509; // radius of the wheels in m
 const int MAX_RPM		= 150;
-const int LEVELS		= 10;
+const int MIN_LINEAR_RPM	= 20;
+const int MIN_ANGULAR_RPM	= 10;
+
+const int LEVELS		= 5;
 
 const float MAX_LINEAR_SPEED	= (MAX_RPM * (2*M_PI)/60) * WHEEL_RADIUS ; // m/s
 const float MAX_ANGULAR_SPEED	= MAX_LINEAR_SPEED / (DIST_BETWEEN_WHEELS/2); //rad/s
 
-const float MIN_LINEAR_SPEED = MAX_LINEAR_SPEED / LEVELS;
+const float MIN_LINEAR_SPEED = 20;
 const float MIN_ANGULAR_SPEED = MAX_ANGULAR_SPEED / LEVELS;
+
+const int LINEAR_RPM_VALUES[LEVELS]	= {20,40,60,80,100};
+const int ANGULAR_RPM_VALUES[LEVELS]	= {10,25,40,55,70};
+
+inline float to_meter_sec(int rpm)
+{
+    return (rpm * (2*M_PI)/60) * WHEEL_RADIUS;
+}
+
+inline float to_rad_sec(int rpm)
+{
+    return to_meter_sec(rpm) / (DIST_BETWEEN_WHEELS/2);
+}
 
 int main(int argc, char **argv)
 {
-    cv::Mat static_frame(height, width, CV_8UC3, cv::Scalar(150,150,150));
+    cv::Mat static_frame(height, width, CV_8UC3, cv::Scalar(153,76,0)); //BGR
     cv::Mat text_frame; 
     cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 
@@ -47,7 +64,7 @@ int main(int argc, char **argv)
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(20);
 
     geometry_msgs::Twist msg;
     
@@ -89,7 +106,7 @@ int main(int argc, char **argv)
 	switch(key)
 	{ 
 	    case 'a':
-		speed = MIN_ANGULAR_SPEED * speed_lvl;
+		speed = to_rad_sec( ANGULAR_RPM_VALUES[speed_lvl - 1] );
 
 		msg.linear.x	= 0;
 		msg.angular.z	= speed;
@@ -98,7 +115,7 @@ int main(int argc, char **argv)
 		units_msg = "rad/s";
 		break;
 	    case 'd':
-		speed = -MIN_ANGULAR_SPEED * speed_lvl;
+		speed = to_rad_sec( -ANGULAR_RPM_VALUES[speed_lvl - 1] );
 
 		msg.linear.x	= 0;
 		msg.angular.z	= speed;
@@ -107,7 +124,7 @@ int main(int argc, char **argv)
 		units_msg = "rad/s";
 		break;
 	    case 's':
-		speed = -MIN_LINEAR_SPEED * speed_lvl;
+		speed = to_meter_sec( -LINEAR_RPM_VALUES[speed_lvl - 1] );
 
 		msg.linear.x	= speed; 
 		msg.angular.z	= 0;
@@ -116,7 +133,7 @@ int main(int argc, char **argv)
 		units_msg = "m/s";
 		break;
 	    case 'w':
-		speed = MIN_LINEAR_SPEED * speed_lvl;
+		speed = to_meter_sec( LINEAR_RPM_VALUES[speed_lvl - 1] );
 
 		msg.linear.x	= speed;
 		msg.angular.z	= 0;
@@ -134,6 +151,8 @@ int main(int argc, char **argv)
 		break;
 	    case -1:
 		//Do nothing because no key was pressed
+		//This value may change with versions
+		//different than 4.x
 		break;
 	    case -23:
 		// Do nothing alt key was pressed
